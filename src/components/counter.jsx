231348f31d7c38d59af42b81287d4be1aa692d2b
Omar <omarabdeljelil@gmail.com>
2018-12-09 15:26:19 -0500

    init commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>zero</h1>
+        <button>increment</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
